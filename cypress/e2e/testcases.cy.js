bb14fb5b478b998f40aee4f8d0ffd776b3ebcf76
AhmadIhsan999 <ahmadahsan282@gmail.com.com>
2023-09-18 10:37:19 +0300

    update test

--- cypress/e2e/testcases.cy.js
+++ cypress/e2e/testcases.cy.js
@@ -1,27 +1,65 @@
 /// <reference types= "cypress" />
-Cypress.on('uncaught:exception',(err,runnable)=>{
-  return false
-}
-)
-describe('Aspire test cases', () => {
-  it('randomly enter the website arabic or english', () => {
-    let website = ["https://global.almosafer.com/ar" , "https://global.almosafer.com/en"];
-    let randomindex = Math.floor(Math.random()*website.length)
-    cy.visit(website[randomindex]);
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
 
-    let ArabiCities =["جدة","دبي"]
-    let ArabicRAndoomIndex = Math.floor(Math.random()*ArabiCities.length)
-    let EngCities =["riyadh","dubai","jeddah"]
-    let EngRAndoomIndex = Math.floor(Math.random()*EngCities.length)
-    
-  cy.get('.cta__saudi').click()
-  cy.get('#uncontrolled-tab-example-tab-hotels').click()
+Cypress.Commands.add("visiturl", () => {
+  let Websites = [
+    "https://global.almosafer.com/ar",
+    "https://global.almosafer.com/en",
+  ];
 
-  if (randomindex==0) {
-cy.get('[data-testid="AutoCompleteInput"]').type(ArabiCities[ArabicRAndoomIndex])
-}
-  else {
-  cy.get('[data-testid="AutoCompleteInput"]').type(EngCities[EngRAndoomIndex])
-  }
-  })
-})
+  let RandomIndex = Math.floor(Math.random() * Websites.length);
+  cy.visit(Websites[RandomIndex]);
+
+  cy.get(".cta__saudi").click();
+});
+
+describe("Aspire Test cases ", () => {
+  const TheDate = new Date();
+
+  const today_date = TheDate.getDate();
+  const expectedDepatureDate = today_date + 1;
+  const expectedreturnDate = today_date + 2;
+
+  console.log(TheDate);
+
+  it.skip("Randomly enter the website arabic or english ", () => {
+    let Websites = [
+      "https://global.almosafer.com/ar",
+      "https://global.almosafer.com/en",
+    ];
+
+    let RandomIndex = Math.floor(Math.random() * Websites.length);
+    cy.visit(Websites[RandomIndex]);
+
+    let ArabicCities = ["جدة", "دبي"];
+    let ArabicRandomIndex = Math.floor(Math.random() * ArabicCities.length);
+    let englishCities = ["riyadh", "dubai", "jeddah"];
+    let EnglishRandomIndex = Math.floor(Math.random() * englishCities.length);
+
+    cy.get(".cta__saudi").click();
+    cy.get("#uncontrolled-tab-example-tab-hotels").click();
+    if (RandomIndex == 0) {
+      cy.get('[data-testid="AutoCompleteInput"]').type(
+        ArabicCities[ArabicRandomIndex]
+      );
+    } else {
+      cy.get('[data-testid="AutoCompleteInput"]').type(
+        englishCities[EnglishRandomIndex]
+      );
+    }
+  });
+
+  it("test the depature date + the return date ", () => {
+    cy.visiturl();
+
+    cy.get('[data-testid="FlightSearchBox__FromDateButton"] > .sc-eSePXt')
+      .invoke("text")
+      .then((elementText) => {
+        expect(expectedDepatureDate).to.eql(parseInt(elementText.trim()));
+      });
+  });
+});
